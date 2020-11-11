f641e4ed85a7fa5130a67427e0c940b3b6ae31a8
oluwatobi oyejide <moyejide2016@gmail.com>
2020-11-11 19:36:56 +0100

    setting the codebase up for prod

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const cors = require("cors");
 let { persons } = require("./src/persons");
+
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
-app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
+
+morgan.token("body", function(req, res){
+    return JSON.stringify(req.body);
+})
+app.use(morgan( ":method :url :status :res[content-length] - :response-time ms :body" ));
+
 
 app.get("/api/info", (req, res) => {
   let recievedTime = new Date();
@@ -21,12 +28,11 @@ app.get("/api/info", (req, res) => {
 });
 
 app.get("/api/persons", (req, res) => {
-  res.status(200);
-  res.json(persons);
+  res.status(200).json(persons).end();
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = Number(req.params.id);
   const contact = persons.find((contact) => contact.id === id);
 
   if (contact) {
@@ -37,25 +43,42 @@ app.get("/api/persons/:id", (req, res) => {
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  persons = persons.filter(person => person !== id)
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id)
 
-  res.status(204).end;
+  res.status(204).end();
 
 });
 
 app.post("/api/persons", (req, res) => {
-    const person = req.body;
+    const recievedPersonData = req.body;
 
-    if(person){
-        persons = persons.concat(person);
-        console.log(persons);
-        res.status(200).end();
-    } else {
+    if(!recievedPersonData){
         res.status(400).json({
             error: "No content"
-        })
+        });
+        return;
     }
+    if(!recievedPersonData.name || !recievedPersonData.number){
+        res.status(400).json({
+            error: "Incomplete content, 'name' or 'number' is missing"
+        });
+        return;
+    }
+    if(persons.some(person => person.number === recievedPersonData.name)){
+        res.status(400).json({
+            error: `'name' must be unique, person with name: "${recievedPersonData.name}" already exists`
+        });
+        return;
+    }
+
+    const processedPersonData = {
+        ...recievedPersonData,
+        id: Math.random() * 1000000
+    }
+    persons = persons.concat(processedPersonData);
+    res.status(200).send(processedPersonData);
+    
 })
 
 app.listen(PORT, () => {
