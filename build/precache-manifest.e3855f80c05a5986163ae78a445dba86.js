3112d1cb7c0a42d84de5a237ee21e997832d564e
oluwatobi oyejide <moyejide2016@gmail.com>
2020-11-11 20:16:46 +0100

    server-deploy-uibuild

---
+++ build/precache-manifest.e3855f80c05a5986163ae78a445dba86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482b09acf5bb2e9dbedb50375009fd13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad7666c756f34bf8e66",
+    "url": "/static/css/main.c153f074.chunk.css"
+  },
+  {
+    "revision": "bb16f0b803314f65feca",
+    "url": "/static/js/2.e0bb2463.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0bb2463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad7666c756f34bf8e66",
+    "url": "/static/js/main.b52b9b88.chunk.js"
+  },
+  {
+    "revision": "aa03e00c16e389062aec",
+    "url": "/static/js/runtime-main.328dad9e.js"
+  }
+]);
